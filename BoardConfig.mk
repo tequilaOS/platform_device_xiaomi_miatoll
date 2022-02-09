a36eda9606d61a4262ecec2dc843ac2a137c3ed0
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-02-09 21:02:54 +0100

    miatoll: Reboot to recovery if init fails
    Change-Id: I1d8f38620caa7b003911ff30f69ed7973c100e6d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,6 +111,7 @@ BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=1
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := curtana,excalibur,gram,joyeuse,miatoll
