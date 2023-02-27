5b5698c6b7ee386ccf0bc3a01b1dbcf979576de5
LuK1337 <priv.luk@gmail.com>
2023-02-27 18:05:50 +0800

    miatoll: overlay: Configure aux camera for Aperture
    Change-Id: I543f078dd4dba30e1b22737d5c22d591e3fb34e6
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- device.mk
+++ device.mk
@@ -271,6 +271,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
+    ApertureOverlay \
     CarrierConfigResCommon \
     SM6250Frameworks \
     SM6250SystemUI \
