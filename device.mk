ca40c8e2ccd00cf5868f5a465675f6874d03f6d8
clarencelol <clarencekuiek@proton.me>
2024-01-28 11:53:56 +0800

    miatoll: Switch back to OSS Audio
    * Import stock changes over latest CLO audio configs
    * Build libssr from source and enable few flags
    
    This reverts commit 577f1b092868c8651068db0854f75de97d515a37.

--- device.mk
+++ device.mk
@@ -35,10 +35,12 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
+    audio.primary.atoll \
     audio.r_submix.default \
     audio.usb.default \
 
 PRODUCT_PACKAGES += \
+    liba2dpoffload \
     libbatterylistener \
     libcomprcapture \
     libexthwplugin \
@@ -49,6 +51,7 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing \
     libsndmonitor \
     libspkrprot \
+    libssrec \
     libtinycompress \
     libvolumelistener
 
@@ -56,6 +59,7 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
 PRODUCT_COPY_FILES += \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
@@ -223,10 +227,6 @@ PRODUCT_COPY_FILES += \
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
-
-# Libshims
-PRODUCT_PACKAGES += \
-    fakelogprint
 
 # Lineage Health
 PRODUCT_PACKAGES += \
